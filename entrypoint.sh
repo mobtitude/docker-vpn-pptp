c60f83f1357edb944795bbbaa97311003cfeb502
Dustin Blackman <dev@dustinblackman.com>
2018-03-06 11:23:58 -0500

    fix iptables

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,9 @@ sysctl -w net.ipv4.ip_forward=1
 # configure firewall
 iptables -t nat -A POSTROUTING -s 10.99.99.0/24 ! -d 10.99.99.0/24 -j MASQUERADE
 iptables -A FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -A INPUT -i ppp0 -j ACCEPT
+iptables -A OUTPUT -o ppp0 -j ACCEPT
+iptables -A FORWARD -i ppp0 -j ACCEPT
+iptables -A FORWARD -o ppp0 -j ACCEPT
 
 exec "$@"
